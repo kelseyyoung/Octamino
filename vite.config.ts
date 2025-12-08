17c69f75f538e8db60444d868d421a9f2c2bc88a
Kelsey Young <kelseyyoung4@gmail.com>
2025-12-07 20:25:18 -0800

    Update NotTetris refs to Octamino

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
-  const base = command === "build" ? "/NotTetris/" : "/";
+  const base = command === "build" ? "/Octamino/" : "/";
 
   return {
     plugins: [react()],
