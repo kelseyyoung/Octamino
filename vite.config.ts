e112b0912716c9b626522e667d68fcbe6a8a11f9
Kelsey Young <kelseyyoung4@gmail.com>
2025-12-25 19:08:58 -0800

    Change build base back to /

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const base = command === "build" ? "/Octamino/" : "/";
+export default defineConfig(() => {
+  // const base = command === "build" ? "/Octamino/" : "/";
+  const base = "/";
 
   return {
     plugins: [react()],
